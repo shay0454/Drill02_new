638f1e70c955e116956fa862b89f97ab1b1e66cb
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-09-21 10:02:38 +0900

    draw character and grass

--- character_circle_and_rectangle.py
+++ character_circle_and_rectangle.py
@@ -1,4 +1,5 @@
 from pico2d import*
+import math
 open_canvas()
 grass=load_image("grass.png")
 character=load_image("character.png")
@@ -7,9 +8,15 @@ def run_rectangle():
     pass
 def run_circle():
     print('circle')
+    #그림 그림
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(400,90)
+    delay(1)
     pass
 while True:
     run_rectangle()
     run_circle()
+    break
 
 close_canvas()
