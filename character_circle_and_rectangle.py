760aa8c78f2ea7a89397363c3c12e922840a4e78
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-09-21 10:43:10 +0900

    change set points to {x,y}

--- character_circle_and_rectangle.py
+++ character_circle_and_rectangle.py
@@ -12,9 +12,9 @@ def run_rectangle():
     print('rect')
     x,y=400,90
     for x in range(50,750+1,10):render_frame(x,y)
-    for y in range(90,510+1,10):render_frame(750,y)
-    for x in range(750,50-1,-10):render_frame(x,550)
-    for y in range(510,90-1,-10):render_frame(50,y)
+    for y in range(90,510+1,10):render_frame(x,y)
+    for x in range(750,50-1,-10):render_frame(x,y)
+    for y in range(510,90-1,-10):render_frame(x,y)
 def run_circle():
     print('circle')
     #그림 그림
