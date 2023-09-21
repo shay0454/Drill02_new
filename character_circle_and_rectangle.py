0ac0315715666b101cb911dca613075b7303e20a
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-09-21 09:58:58 +0900

    무한루프 완성

--- character_circle_and_rectangle.py
+++ character_circle_and_rectangle.py
@@ -2,4 +2,14 @@ from pico2d import*
 open_canvas()
 grass=load_image("grass.png")
 character=load_image("character.png")
+def run_rectangle():
+    print('rect')
+    pass
+def run_circle():
+    print('circle')
+    pass
+while True:
+    run_rectangle()
+    run_circle()
+
 close_canvas()
